41430bd62d1ca53eaadcf5047fe732d6eea1c1ea
Rahul Nema <43380701+rahulnema0411@users.noreply.github.com>
2020-11-25 17:56:12 +0530

    final

--- Assets/bowling.cs
+++ Assets/bowling.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class bowling : MonoBehaviour {
     
     public float speed = 20f;
-    private float initialScale = 0.1f;
     // Start is called before the first frame update
     void Start() {
         
@@ -14,17 +13,13 @@ public class bowling : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         
-        if(initialScale > 0.05f) {
-            initialScale -= 0.001f;
-        } else {
-            initialScale += 0.001f;
-        }
-
-        
-        Vector3 scale = new Vector3(initialScale,initialScale,transform.localScale.z);
-        
-        transform.localScale = scale;
         transform.Translate(0, speed*Time.deltaTime, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("bat")) {
+            Debug.Log("hit ball");
+        }
     }
 
 }
