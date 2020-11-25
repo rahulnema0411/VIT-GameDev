7fb817b55da7eff6cf0dde8e0f9f21d3fe2b8c17
Rahul Nema <43380701+rahulnema0411@users.noreply.github.com>
2020-11-25 17:09:49 +0530

    Level 1 achieved

--- Assets/bowling.cs
+++ Assets/bowling.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class bowling : MonoBehaviour {
     
     public float speed = 20f;
-    
+    private float initialScale = 0.1f;
     // Start is called before the first frame update
     void Start() {
         
@@ -13,6 +13,17 @@ public class bowling : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        
+        if(initialScale > 0.01f) {
+            initialScale -= 0.001f;
+        } else {
+            initialScale += 0.001f;
+        }
+
+        
+        Vector3 scale = new Vector3(initialScale,initialScale,transform.localScale.z);
+        
+        transform.localScale = scale;
         transform.Translate(0, speed*Time.deltaTime, 0);
     }
 
---
+++ Assets/bowlingMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bowlingMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text BowlText;
+
+    private string selectedBowltext = "";
+
+    public void setBowl(int type)
+    {
+        if (type == 1)
+        {
+            selectedBowltext += "fast ";
+        }
+        else
+        {
+            selectedBowltext += "spin ";
+        }
+    }
+
+    public void setBowlType(int n)
+    {
+        selectedBowltext += n;
+
+        //BowlText.enabled = true;
+        BowlText.text = selectedBowltext;
+    }
+}
